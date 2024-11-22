e8a74a271a705bb0bb5e8153e09922bceef4a816
Eldrich Marin Flores <eldrich.marin@utp.edu.co>
2024-11-22 00:24:23 -0600

    se modifico BuyCar hasta verificar el stock no sobrepase

--- website-dolibarr/src/BuyCar.jsx
+++ website-dolibarr/src/BuyCar.jsx
@@ -1,72 +1,99 @@
 // App.jsx
 import { button } from "@material-tailwind/react";
 import React, { useState } from "react";
+import Footer from "./footer";
 
 const products = [
-  { id: 1, name: "Producto A", price: 100, note_public: "Es un producto bonito." },
-  { id: 2, name: "Producto B", price: 150, note_public: "Es un producto bonito." },
-  { id: 3, name: "Producto C", price: 200, note_public: "Es un producto bonito." },
+  { id: 2, name: "Producto B", price: 150, note_public: "Es un producto bonito.", URL: "https://m.media-amazon.com/images/I/61sgXDWDjVL._AC_AA180_.jpg", stock: 5 },
+  { id: 3, name: "Producto C", price: 200, note_public: "Es un producto bonito.", URL: "https://m.media-amazon.com/images/I/61sgXDWDjVL._AC_AA180_.jpg", stock: 10 },
+  { id: 1, name: "Producto A", price: 100, note_public: "Es un producto bonito.", URL: "https://m.media-amazon.com/images/I/61sgXDWDjVL._AC_AA180_.jpg", stock: 10 },
 ];
 
 
 const BuyCar = () => {
+
+
+
   const [cart, setCart] = useState([]);
 
   const addToCart = (product) => {
-    setCart([...cart, product]);
+    let count_products = cart.filter(item => item.id === product.id).length;
+    console.log(count_products);
+    if (count_products < product.stock) {
+      setCart([...cart, product]);
+    }else{
+      alert("Stock fuera de existencia");
+    }
+  };
+  const delete_product = (id) => {
+    console.log("eliminado");
+    let p = setCart(prevItems => prevItems.filter(cart => cart.id !== id));
+    console.log(p);
   };
 
   return (
-    <div className="min-h-screen bg-gray-200 p-4">
-      {/* productos */}
-
-      <div className="flex gap-12">
-        {/* Lista de productos */}
-        <div className="w-3/4">
-          <h1 className="text-3xl font-bold text-center">Carrito de compras</h1>
-          <h2 className="text-2xl font-semibold mb-4">
-            Tus Productos Favoritos
-          </h2>
-          <div className="space-y-4">
-            {products.map((product) => (
-              <div
-                key={product.id}
-                className="p-4 bg-white shadow rounded flex justify-between items-center"
-              >
-                <div>
-                  <h3 className="font-bold">{product.name}</h3>
-                  <p className="text-gray-500">${product.price}</p>
-                  <span className="text-gray-500">{product.note_public}</span>
-                </div>
-                <label></label>
-                <button
-                  onClick={() => addToCart(product)}
-                  className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+    <div>
+      <div className="min-h-screen bg-gray-200 p-4">
+        {/* productos */}
+        <div className="flex gap-12">
+          {/* Lista de productos */}
+          <div className="w-3/4">
+            <h1 className="text-3xl font-bold text-center">Carrito de compras</h1>
+            <h2 className="text-2xl font-semibold mb-4">
+              Tus Productos Favoritos
+            </h2>
+            <div className="space-y-4">
+              {products.map((product) => (
+                <div
+                  key={product.id}
+                  className="p-4 bg-white shadow rounded flex justify-between items-center"
                 >
-                  Agregar al carrito
-                </button>
-              </div>
-            ))}
+                  <img src={product.URL} alt={product.name} />
+                  <div>
+                    <h3 className="font-bold">{product.name}</h3>
+                    <p className="text-gray-500">${product.price}</p>
+                    <span className="text-gray-500">{product.note_public}</span><br />
+                    <span className="text-gray-400">Stock: {product.stock}</span>
+                  </div>
+                  <button
+                    onClick={() => delete_product(product.id)}
+                    className="hover:underline text-blue-500 font-normal text-md">
+                    Eliminar
+                  </button>
+                  <button
+                    onClick={() => addToCart(product)}
+                    className="bg-yellow-300 text-black px-5 py-1 rounded-2xl hover:bg-yellow-500 border-2"
+                  >
+                    Agregar al carrito
+                  </button>
+                </div>
+              ))}
+            </div>
+          </div>
+          {/* Carrito */}
+          <div className="w-auto h-40 bg-white rounded-md p-4">
+            <h2 className="font-base h-auto text-xs sm:text-sm md:text-md xl:text-xl  text-black m-2">
+              Subtotal ({cart.length} productos)
+              <span className="font-semibold">
+                : ${cart.reduce((total, item) => total + item.price, 0)}
+              </span>
+            </h2>
+            <div className="h-80">
+              {cart.length === 0 ? (
+                <p className="font-semibold text-xs sm:text-xs md:text-md xl:text-2xl text-black mx-6">
+                  El carrito está vacío
+                </p>
+              ) : (
+                <div className="text-center">
+                  <button className="font-normal hover:bg-yellow-400 text-xs sm:text-xs md:text-md xl:text-base md:m-5 text-black bg-yellow-300 rounded-2xl py-1 px-12 text-center border-2 ">Proceder al pago</button>
+                </div>
+              )}
+            </div>
           </div>
         </div>
-        {/* Carrito */}
-        <div className="w-1/4 bg-white rounded-md p-2">
-          <h2 className="font-semibold text-xs sm:text-sm md:text-md xl:text-2xl  text-black m-2">
-            Subtotal ({cart.length} productos): $
-            {cart.reduce((total, item) => total + item.price, 0)}
-          </h2>
-          <div className="space-y-6">
-            {cart.length === 0 ? (
-              <p className="font-semibold text-xs sm:text-xs md:text-md xl:text-2xl text-black m-4">
-                El carrito está vacío
-              </p>
-            ) : (
-              <div className="font-semibold text-xs sm:text-xs md:text-md xl:text-2xl md:m-5 text-black my-5 mx-2 bg-yellow-400 rounded-md py-1 text-center">
-                <button className="font-medium">Proceder al pago</button>
-              </div>
-            )}
-          </div>
-        </div>
+      </div>
+      <div className="bg-gray-200 pt-5">
+        <Footer></Footer>
       </div>
     </div>
   );
